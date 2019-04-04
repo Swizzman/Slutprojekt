f8f6ba84118531aecc71ebf0f22820b100047696
Swizzman <swizzman123@gmail.com>
2019-04-04 15:02:05 +0200

    Got Rotation working

--- Car gun Materix Stuff/Assets/PlayerController.cs
+++ Car gun Materix Stuff/Assets/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿        using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 
     public GameObject player;
     public GameObject testSpawn;
+    public GameObject cannon;
+
     Vector2 finalPos;
 
     Rigidbody2D rb;
@@ -15,27 +17,58 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        //Konverterar muspositionen till kordinater baserat på kameran
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward);
+
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = 10;
-            //Konverterar muspositionen till kordinater baserat på kameran
-            Vector3 targetPos = Camera.main.ScreenToWorldPoint(mousePos);
+            
 
+            mousePos.z = 10;
+            Vector3 targetPos = mousePos;
             //GameObject test = Instantiate(testSpawn, targetPos, Quaternion.identity);
 
             rb.GetComponent<Rigidbody2D>().AddForce(targetPos * -1 * 30);
             
         }
 
-        
-        
+        //mousePos.z = 10;
+        //Vector3 targetPos2 = Camera.main.ScreenToWorldPoint(mousePos2);
+        //Vector2 offset = new Vector2(targetPos2.x - cannon.transform.parent.transform.position.x, targetPos2.y - cannon.transform.parent.transform.position.y);
+
+        Vector3 cannonPos = cannon.transform.parent.transform.position;
+        Vector3 offset = mousePos - cannonPos;
+
+
+        //Av någon jävla anledning behövs Atan 2 för att räkna ut rätt
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+        print(angle);
+
+        if (cannon.transform.parent.transform.rotation.z < -90)
+        {
+            //cannon.transform.parent.eulerAngles = new Vector3(0, 0, -90);
+            cannon.transform.parent.eulerAngles = new Vector3(0, 0, angle);
+        }
+        else if (cannon.transform.parent.transform.rotation.z > 0)
+        {
+            //cannon.transform.parent.eulerAngles = new Vector3(0, 0, 0);
+            cannon.transform.parent.eulerAngles = new Vector3(0, 0, angle);
+        }
+        else
+        {
+            cannon.transform.parent.eulerAngles = new Vector3(0, 0, angle);
+
+        }
+
+        //print(cannon.transform.parent.rotation.z);
+
     }
 }
